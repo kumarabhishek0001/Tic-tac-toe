d8526af03af7f4ed2d11c4719520ffb78a479336
abhishek.dev <157294607+kumarabhishek0001@users.noreply.github.com>
2025-08-14 23:52:44 +0530

    Refactor position validation and output handling in Tic-Tac-Toe

--- main2.cpp
+++ main2.cpp
@@ -14,6 +14,17 @@ bool availablePositions(int position){
     return false;
 }
 
+//if the position exists check if position taken or not
+bool positionTaken(int position, map <int, char> occupiedPositions){
+
+    if(occupiedPositions[position] == 0){//this means it is not taken
+        return false;
+    }
+
+    else return true;
+    
+}
+
 
 // assigns X/O to certain possition
 void assigningPositions(int &count, int currentPosition, map <int, char> &mpp){
@@ -26,6 +37,23 @@ void assigningPositions(int &count, int currentPosition, map <int, char> &mpp){
     count++;
 }
 
+
+//getting output after every new entry
+void printingGameState(map <int, char> occupiedPositions){
+
+    const int arr[9] = {0, 1, 2, 11, 12, 13, 21, 22, 23};
+
+    for(int i=0; i<9; i++){
+        if(occupiedPositions[arr[i]] != 0){
+            cout<<occupiedPositions[arr[i]]<<"|";
+        }
+
+        else cout<<"_|";
+
+        if(i%3 == 2 ) cout<<endl;
+    }
+}
+
 int main(){
     int count = 0; // which user will take the move (count%2==0)-> O's move if not then X's move 
     char user = 'X';
@@ -34,18 +62,30 @@ int main(){
     map <int, char> occupiedPositions;
 
     for(int i=0; i<3; i++){
+
         if(count%2==0) user='O';
         else user='X';  // to print in console whose turn is it
         cout<<user<<" enter the position: ";
         cin>>position;
 
-        if(availablePositions(position)) assigningPositions(count, position, occupiedPositions);
+        //positions checked assigned in map
+        if(availablePositions(position)){
+
+            if(!positionTaken(position, occupiedPositions)) assigningPositions(count, position, occupiedPositions);
+            else cout<<"Position is already taken!! Please enter the postion again"<<endl;                   
+        } 
             
-        else cout<<"invalid input!!";
-        
+        else {
+            cout<<"invalid input!! Please enter the value again: "<<endl;
+        }
+
+        //getting the state of game after value is assigned
+        printingGameState(occupiedPositions);
+
     }
 
     //getting output
+    cout<<"--------mapvalue--------"<<endl;
     for(auto it: occupiedPositions){
         cout<<it.first<<"->"<<it.second<<endl;
     }
